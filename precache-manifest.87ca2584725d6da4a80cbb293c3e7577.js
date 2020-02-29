4394ec3bf6fb31cd88af5573f00642848f2c2d27
Devraj Singh <devraj.singh.rawat83@gmail.com>
2020-02-29 14:22:36 +0100

    Updates

---
+++ precache-manifest.87ca2584725d6da4a80cbb293c3e7577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbb324524ba523a81f23a9c9b8d8ec6",
+    "url": "/reactDemos/index.html"
+  },
+  {
+    "revision": "791d9968043af17b09a0",
+    "url": "/reactDemos/static/css/main.57ce8dbe.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/reactDemos/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactDemos/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791d9968043af17b09a0",
+    "url": "/reactDemos/static/js/main.74be5221.chunk.js"
+  },
+  {
+    "revision": "65bcad7dc0c3dcffca3e",
+    "url": "/reactDemos/static/js/runtime-main.8ac2837f.js"
+  }
+]);
